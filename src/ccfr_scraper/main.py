f6e8db62d412b27f34cc70433bb89a7aa22317af
kat-kel <klarinet28@gmail.com>
2025-05-14 14:12:33 +0200

    move cli commands

--- src/ccfr_scraper/main.py
+++ src/ccfr_scraper/main.py
@@ -1,17 +1,6 @@
 import click
-from rich.progress import (
-    Progress,
-    BarColumn,
-    SpinnerColumn,
-    TimeElapsedColumn,
-    TextColumn,
-    MofNCompleteColumn,
-)
-from rich.console import Console
-import casanova
 
-from ccfr_scraper.models import Description
-from ccfr_scraper.scrape import scrape_page
+from ccfr_scraper.commands import from_file_command, from_url_command
 
 
 @click.group
@@ -25,17 +14,7 @@ a document in the Catalogue collectif de France using the notice's URL."""
 @click.argument("url")
 def from_url(url: str):
     """Scrape 1 URL."""
-    console = Console()
-    with Progress(
-        TextColumn("{task.description}"),
-        SpinnerColumn(),
-        TimeElapsedColumn(),
-        console=console,
-    ) as p:
-        _ = p.add_task("Scraping...")
-        console.print(url)
-        modelled_data = scrape_page(url=url)
-        console.print(modelled_data)
+    from_url_command(url=url)
 
 
 @cli.command("file")
@@ -50,32 +29,7 @@ def from_url(url: str):
 )
 def from_file(infile: str, column: str, outfile: str):
     """Scrape a set of URLs from a CSV file."""
-    console = Console()
-    addendum = list(Description.__annotations__.keys())
-    total = casanova.count(infile)
-    with (
-        open(infile) as f,
-        open(outfile, "w") as of,
-        Progress(
-            TextColumn("{task.description}"),
-            BarColumn(),
-            MofNCompleteColumn(),
-            TimeElapsedColumn(),
-            console=console,
-        ) as p,
-    ):
-        t = p.add_task("Scraping", total=total)
-        enricher = casanova.enricher(f, of, add=addendum)
-        for row, url in enricher.cells(column=column, with_rows=True):
-            console.rule("Current")
-            console.print(url)
-            modelled_data = scrape_page(url=url)
-            console.clear()
-            console.rule("Result")
-            console.print(url)
-            console.print(modelled_data)
-            enricher.writerow(row, add=modelled_data.model_dump().values())
-            p.advance(t)
+    from_file_command(infile=infile, column=column, outfile=outfile)
 
 
 if __name__ == "__main__":
